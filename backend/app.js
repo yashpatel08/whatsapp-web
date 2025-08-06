62320bc9e5d5bcd3ba232c95082af1571f4ce7ea
Yash <yashpatel54257@gmail.com>
2025-08-06 23:19:38 +0530

    ...

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,11 @@ const webhookRoutes = require('./routes/webhookRoutes');
 const messageRoutes = require('./routes/messageRoutes');
 
 const app = express();
-
-app.use(cors());
+app.use(cors({
+    origin: '*', // Allow all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 app.use('/api/webhook', webhookRoutes);
 app.use('/api/messages', messageRoutes);
